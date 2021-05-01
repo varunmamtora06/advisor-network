dee2e84671200e47cf8357e3111b104632fdeed8
Varun Mamtora <varunmamtora@gmail.com>
2021-05-01 19:56:11 +0530

    add advisor and booking api endpoints

--- advisor/main/urls.py
+++ advisor/main/urls.py
@@ -7,10 +7,16 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('admin/advisor/',views.AdvisorAddApi.as_view()),
 
     path('user/register/',views.RegisterNewUserView.as_view()),
     # path('api-newuser-login',views.LoginNewUserView.as_view()),
     path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('admin/advisor/',views.AdvisorApi.as_view()),
+    path('user/advisor/', views.AdvisorApi.as_view()),
+    # path('user/advisor/<int:pk>/', views.AdvisorApi.as_view()),
+
+    path('user/<int:user_id>/advisor/<int:advisor_id>/', views.BookingApi.as_view()),
+    path('user/<int:user_id>/advisor/booking/', views.BookingApi.as_view()),
 ]
--- advisor/main/views.py
+++ advisor/main/views.py
@@ -22,7 +22,18 @@ class RegisterNewUserView(APIView):
         }, status=200)
 
 
-class AdvisorAddApi(APIView):
+class AdvisorApi(APIView):
+
+    def get(self, request, pk=None):
+        if pk == None:
+            advisors = Advisor.objects.all()
+            serializer = AdvisorSerializor(advisors, many=True)
+            return Response(serializer.data)
+        else:
+            advisor = Advisor.objects.get(id=pk)
+            serializer = AdvisorSerializor(advisor)
+            return Response(serializer.data)
+
     def post(self, request):
         serializer = AdvisorSerializor(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -31,3 +42,32 @@ class AdvisorAddApi(APIView):
         return Response({
             'mssg':'Data entered success'
         },status=200)
+
+import json
+class BookingApi(APIView):
+
+    def get(self, request, user_id=None):
+        bookings = Booking.objects.filter(by_user=NewUserModel.objects.get(id=user_id))
+        serializer = BookingSerializor(bookings, many=True)
+
+        # resp = {}
+        advisors=[]
+        # dic = {}
+        for booking in bookings:
+            # advisors.append(booking.advisor_booked)
+            
+            # dic.update({"Advisor Name":booking.advisor_booked.advisor_name,"Advisor pic":booking.advisor_booked.advisor_pic.url,"Advisor id":booking.advisor_booked.id})
+            # dic.update({"Advisor pic":booking.advisor_booked.advisor_pic.url})
+            # dic.update({"Advisor id":booking.advisor_booked.id})
+            # print(dic)
+            # advisors.append(dic)
+            advisors.append({"Advisor Name":booking.advisor_booked.advisor_name,"Advisor pic":booking.advisor_booked.advisor_pic.url,"Advisor id":booking.advisor_booked.id})
+            print(advisors)
+        print(serializer.data)
+        return Response({"booking_details":serializer.data,"advisor_details":advisors}, status=200)
+
+    def post(self, request, user_id=None, advisor_id=None):
+        serializer = BookingSerializor(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        Booking.objects.create(booking_time=serializer.validated_data['booking_time'],advisor_booked=Advisor.objects.get(id=advisor_id), by_user=NewUserModel.objects.get(id=user_id))
+        return Response(status=200)
