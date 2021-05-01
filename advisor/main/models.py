f76e16f1db7f032f6929f3c320c9108d63b9a8a6
Varun Mamtora <varunmamtora@gmail.com>
2021-05-01 14:47:37 +0530

    make advisor, booking models

--- advisor/main/models.py
+++ advisor/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import models as mod
 # Create your models here.
+
+class Advisor(models.Model):
+    advisor_name = models.CharField(max_length=50)
+    advisor_pic = models.ImageField(upload_to='advisorPics/', blank=True)
+
+class Booking(models.Model):
+    booking_time = models.DateTimeField(auto_now_add=False)
+    advisor_booked = models.ForeignKey(Advisor, on_delete=models.CASCADE)
+    by_user = models.ForeignKey(mod.User, on_delete=models.CASCADE)
