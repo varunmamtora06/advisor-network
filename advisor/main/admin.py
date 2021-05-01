c74f2c2d4f0ffbcdd2e3cfbddc91afc73a682258
Varun Mamtora <varunmamtora@gmail.com>
2021-05-01 18:48:57 +0530

    update admin view

--- advisor/main/admin.py
+++ advisor/main/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(NewUserModel)
-admin.site.register(Advisor)
-admin.site.register(Booking)
+@admin.register(NewUserModel)
+class NewUserAdmin(admin.ModelAdmin):
+    list_display=['id','first_name','email']
+
+@admin.register(Advisor)
+class AdvisorAdmin(admin.ModelAdmin):
+    list_display=['id','advisor_name']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display=['id','booking_time','advisor_booked', 'by_user']
